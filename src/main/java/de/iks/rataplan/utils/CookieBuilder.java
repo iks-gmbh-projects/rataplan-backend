ae2bd42af604d2235d4304c20efe16473d6935cf
PrinzIKS <F.Prinz@iks-gmbh.com>
2020-09-30 09:40:02 +0200

    setting cookie via header-field to add samesite property

--- src/main/java/de/iks/rataplan/utils/CookieBuilder.java
+++ src/main/java/de/iks/rataplan/utils/CookieBuilder.java
@@ -2,9 +2,12 @@ package de.iks.rataplan.utils;
 
 import javax.servlet.http.Cookie;
 
+import org.apache.http.client.protocol.ResponseProcessCookies;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
+
+import java.net.HttpCookie;
 
 @Component
 public class CookieBuilder {
@@ -13,23 +16,8 @@ public class CookieBuilder {
 	
 	@Autowired
 	private Environment env;
-	
-	public Cookie createJWTCookie(String token, boolean logout) {
-		Cookie cookie = new Cookie(JWT_TOKEN, token);
-		cookie.setDomain(env.getProperty("rataplan.backend.domain"));
-		cookie.setPath("/");
-		cookie.setHttpOnly(true);
-		cookie.setMaxAge(60000);
-		
-		if ("true".equals(env.getProperty("RATAPLAN.PROD"))) {
-			cookie.setSecure(true);
-		}
-		
-		if (!logout) {
-			cookie.setMaxAge(60000);
-		} else {
-			cookie.setMaxAge(0);
-		}
-		return cookie;
+
+	public String generateCookieValue(String token, boolean logout) {
+		return JWT_TOKEN + "=" + token + "; Max-Age=" + (logout ? 0 : 60000) + "; Domain=" + env.getProperty("rataplan.backend.domain") + "; Path=/; " + ("true".equals(env.getProperty("RATAPLAN.PROD")) ? "Secure" : "") + "; HttpOnly; SameSite=" + ("true".equals(env.getProperty("RATAPLAN.PROD")) ? "none" : "strict");
 	}
 }
